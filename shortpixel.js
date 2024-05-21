c9375219cd7999c04cef1d700d9dcb004a77174a
kosmara <12530117@student.uin-suka.ac.id>
2024-05-21 23:38:28 +0700

    Update shortpixel.js

--- shortpixel.js
+++ shortpixel.js
@@ -5,7 +5,7 @@ $.post('https://api.shortpixel.com/v2/reducer.php',JSON.stringify({
     "resize": 0,
     "cmyk2rgb": 1,
     "refresh": 0,
-    "urllist": ["https://goldenbuzzers.blogspot.com", "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEglOG3CyW7Ng_Y1Upf2-tXlSY9SBOsuVMRcCgItbwWQM-5biCw308PjM0fZ8P3qLkzTp20MMHdAkpyP8iNMeCiI99Fhz0D9_nlqI8UkpkTIVDm0WU_wgPj38VnSwAOUJFB8EMTTjK4V8qEmTojXAI7a7KHD7PjLA4VxyDFlXLHDVd93lGJdtg-KcKK4nQ/s1600/2output.webp"]
+    "urllist": ["https://goldenbuzzers.blogspot.com", "https://blogger.googleusercontent.com"]
 }), function (data) {
     alert('success');
 });
